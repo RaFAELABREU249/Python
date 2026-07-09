e83b21e617ad9caab66fcab026bf8fbbe8ae23d8
RaFAELABREU249 <12400556@aluno.cotemig.com.br>
2026-07-09 15:33:15 -0300

    Add files via upload

---
+++ calculadora mvc/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+from models import db
+from controllers import calculadora_bp
+
+
+def create_app():
+    app = Flask(
+        __name__,
+        static_folder="views/static",
+        template_folder="views/templates",
+    )
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///calculadora.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    app.register_blueprint(calculadora_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ calculadora mvc/controllers/__init__.py
@@ -0,0 +1,3 @@
+from .calculadora_controllers import calculadora_bp
+
+__all__ = ["calculadora_bp"]
